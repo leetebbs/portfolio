8f55923cf8c241f4eeec8089d752473facb086a2
Tebbo <leetebbs@ymail.com>
2023-03-06 13:55:58 +0000

    initial commit
    initial commit

---
+++ src/components/Banner.js
@@ -0,0 +1,89 @@
+import React from "react";
+import image from "../assets/tebbo.png";
+import { FaGithub, FaYoutube, FaTwitter, FaLinkedin } from "react-icons/fa";
+import { TbBrandFiverr } from "react-icons/tb";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
+
+const Banner = () => {
+  return (
+    <section
+      className="min-h-[85vh] lg:min-h-[78vh] flex items-center"
+      id="home"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
+          <div className="flex-1 text-center font-secondary lg:text-left">
+            <motion.h1
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="text-[55px] font-bold leading[0.8] lg:text-[90px] mb-10 mt-10"
+            >
+              LEE <span>TEBBS</span>
+            </motion.h1>
+            <motion.div
+                          variants={fadeIn("up", 0.3)}
+                          initial="hidden"
+                          whileInView={"show"}
+                          viewport={{ once: false, amount: 0.7 }}
+              className="mb-6 text-[25px] lg:text-[45px] font-secondary
+    font-semibold uppercase leading-[1]"
+            >
+              <span className="text-white mr-2">I am a </span>
+              <TypeAnimation
+                sequence={[
+                  "Web Developer",
+                  2000,
+                  "Blockchain Developer",
+                  2000,
+                  "Solidity Developer",
+                  2000,
+                ]}
+                speed={50}
+                className="text-accent"
+                wrapper="span"
+                repeat={Infinity}
+              />
+            </motion.div>
+            <p className="mb-8 max-w-lg mx-auto lg:mx-0">
+              Unlock the full potential of blockchain technology with a skilled
+              and experienced web3 blockchain engineer. From smart contract
+              development to decentralized app creation, I can help bring your
+              blockchain project to life. Hire me as your freelance web3
+              blockchain engineer and let's build a decentralized future
+              together.
+            </p>
+            <div className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
+              <button className="btn btn-lg">Contact me</button>
+              <a href="#" className="text-gradient btn-link">
+                My Portfolio
+              </a>
+            </div>
+            <div className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
+              <a href="#">
+                <FaGithub />
+              </a>
+              <a href="#">
+                <FaLinkedin />
+              </a>
+              <a href="#">
+                <FaTwitter />
+              </a>
+              <a href="#">
+                <TbBrandFiverr />
+              </a>
+            </div>
+          </div>
+          <div className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px] mx-auto">
+            <img src={image} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from'../assets/logo.svg'
+const Header = () => {
+  return(
+    <header>
+<div className="container mx-auto mt-10">
+  <div className='flex justify-between item-center'>
+    <a href="#">
+      {/* <img src={logo} alt='' /> */}
+      <h1 className="text-gradient text-[25px]">LEE</h1>
+      <h1 className='text-[34px] font-semibold'>TEBBS</h1>
+    </a>
+    <button className='btn btn-sm'>Work with me</button>
+  </div>
+</div>
+    </header>
+  )
+ 
+};
+
+export default Header;
