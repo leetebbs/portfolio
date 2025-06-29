96676fc53fa8d39db3d3c951c6c7389dc32e4530
leetebbs <leetebbs@ymail.com>
2025-06-29 14:40:55 +0100

    Add: NoLo to works

--- src/components/Work.js
+++ src/components/Work.js
@@ -163,7 +163,7 @@ const Work = () => {
                 alt=""
               />
               <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
-                <span className="text-gradient">No Loss Yield Farminig Lottery</span>
+                <span className="text-gradient">No Loss Yield Farming Lottery</span>
               </div>
               <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
                 <span className="text-3xl text-white">NoLo Lotto</span>
